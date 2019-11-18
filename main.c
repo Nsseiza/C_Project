6b7c8943e1934444b156eb7b9d183d4041fe2e69
Pedja <predrag.seizovic@panonit.com>
2019-11-18 14:22:31 +0100

    add new function

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+int foo()
+{
+	return 10-9;
+}
 int main()
 {
-	printf("\nFirst C project on Git Project!!!!!\n\n");
+	printf("\nFirst C project%d on Git Project!!!!!\n\n", foo());
 	return 0;
 }
