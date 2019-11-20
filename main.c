872b52ce8e6545309d8dbae75fe5504398fecf8f
Nsseiza <nsseiza@gmail.com>
2019-11-20 11:21:44 +0100

    Add brackets
    bracket odd or even

--- main.c
+++ main.c
@@ -9,8 +9,8 @@ int main(int argc, char* argv[])
 {
 	
 	int x = atoi(argv[1]);	
-	printf("\nFirst odd or even param num_[%d] on Git Project!!!!!\n\n", foo(x));
+	printf("\nFirst (odd or even) param num_[%d] on Git Project!!!!!\n\n", foo(x));
 	int x = atoi(argv[2]);	
-	printf("\nFirst odd or even param num_[%d] on Git Project!!!!!\n\n", foo(x));
+	printf("\nFirst (odd or even) param num_[%d] on Git Project!!!!!\n\n", foo(x));
 	return 0;
 }
