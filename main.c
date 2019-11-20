3ab408c6f6c9770862a5032765b66c0ae143bfa6
Pedja <predrag.seizovic@panonit.com>
2019-11-20 11:04:31 +0100

    main.c

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ int main(int argc, char* argv[])
 {
 	
 	int x = atoi(argv[1]);	
-	printf("\nTest odd or even num_[%d] on Git Project!!!!!\n\n", foo(x));
+	printf("\nFirst odd or even param num_[%d] on Git Project!!!!!\n\n", foo(x));
+	int x = atoi(argv[2]);	
+	printf("\nFirst odd or even param num_[%d] on Git Project!!!!!\n\n", foo(x));
 	return 0;
 }
